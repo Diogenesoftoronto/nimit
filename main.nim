043568c2070ceb7b1e330df685776c6a18744a43
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-03-21 19:13:18 -0400

    Add error called NOGIT
    	-stop the program from executing if no git directory found.

--- main.nim
+++ main.nim
@@ -1,18 +1,29 @@
-import std/rdstdin
-import std/strutils
+import rdstdin
+import strutils
+import os
+import sequtils
+import typetraits
+import system
+
+# Exceptions
+var
+  NOGIT: ref CatchableError
+new(NOGIT)
+NOGIT.msg = "No git directory is present"
 
 proc main() =
+     if dirExists(".git") == false:
+          echo "No Git directory found"
+          raise NOGIT 
+     echo "This is Nimmits"
      try:
-          let input = readLineFromStdin().split(" ")
+          var input: seq[string] = readLineFromStdin("Welcome to Nimits: ").split(" ")
+          echo input
      except IOError, EOFError:
           echo "Could not Read File"
-
-     echo "This is Nimmits"
-     echo input.join(" ")
 
 
 main()
 
-# TODO: Check if we are in a git directory if not create an error
 # TODO: Check the last git commit of the branch using the git log command
 # TODO: Check if it is a valid git commit
