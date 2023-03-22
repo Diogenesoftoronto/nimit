5184d1dcd2b19446c56655a7496cfab8bd3873a7
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-03-22 00:18:36 -0400

    add todos and validate commit!

--- main.nim
+++ main.nim
@@ -1,56 +1,81 @@
-import rdstdin
+# import rdstdin
 import strutils
 import os
+# import re
+# import strformat
 # import sequtils
 import typetraits
 import system
 import osproc
-
+import parsecsv
 
 # Exceptions
 var
-  NOGIT: ref CatchableError
+    NOGIT: ref CatchableError
 new(NOGIT)
 NOGIT.msg = "No git directory is present"
 
 proc getLatestCommit(): string {.raises: [OSError, IOError].} =
-     let cmd = "git log -1 > tmp.commit" 
-     var ok = execCmd(cmd)
-     if ok != 0:
-          raise new(OSError)
-     try:
-          var commit = readFile("tmp.commit")
-          var lineNumber = 0
-          for line in commit.lines:
-               if lineNumber == 4:
-                    result = line
-               inc lineNumber
-     except IOError:
-          raise new(IOError)
-     
+    let cmd = "git log -1 > tmp.commit"
+    var ok = execCmd(cmd)
+    if ok != 0:
+        raise new(OSError)
+    try:
+        var f = open("./tmp.commit")
+        defer: f.close()
+        var lineNumber = 0
+        for line in f.lines:
+            inc lineNumber
+            if lineNumber == 5:
+                result = line
+    except IOError as e:
+        raise e
 
+proc validateCommit(commit: string): bool {.raises: [OSError, IOError, Exception].} =
+    result = false
+    var p: CsvParser
+    var q: CsvParser
+    p.open("./data/pronouns.csv")
+    q.open("./data/verbs.csv")
+    defer: p.close()
+    defer: q.close()
+    # check if sentence begins with a verb
+    p.readHeaderRow
+    q.readHeaderRow
+    while q.readRow():
+        for col in items(q.headers):
+            if col != "Word":
+                continue    
+            var val = q.rowEntry(col)
+            # remove empty spaces from a string
+            if commit.strip.startsWith(val):
+                result = true
+                echo "Valid commit message"
+                break
+    if result == false:
+        echo "Invalid commit message"
+        result = false
+        raise new(OSError)
 
 
 proc main() =
-     try:
-          var input: seq[string] = readLineFromStdin("Welcome to Nimits: ").split(" ")
-          echo input
-     except IOError, EOFError:
-          raise
-     if dirExists(".git") == false:
-          echo "No Git directory found"
-          raise NOGIT 
-     try: 
-          let msg = getLatestCommit()
-          echo msg
-     except OSError, IOError:  
-          raise
+    # try:
+    #     var input: seq[string] = readLineFromStdin("Welcome to Nimits: ").split(" ")
+    # except IOError, EOFError:
+    #     raise
+    if dirExists(".git") == false:
+        echo "No Git directory found"
+        raise NOGIT
+    try:
+        var msg = getLatestCommit()
+        validateCommit(msg)
+    except OSError, IOError:
+        raise
 
 main()
 
-# TODO: Check the last git commit of the branch using the git log command
-# TODO: Check if it is a valid git commit**
-     # check if sentence begins with a verb
-     # check that the verb is in its imperative tense
-     # if not alert the user
-     # if so we are going accept
+# TODO check if the commit is capitalized
+# TODO check if the commit is more than 50 characters
+# TODO check if the commit contains a period at the end
+# TODO check if the commit contains a non imperative verb at the beginning
+# TODO check if the commit contains a pronoun
