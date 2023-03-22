c452fb54437fd7547c2f93cd75b616908896e668
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-03-22 00:26:08 -0400

    add more todos

--- main.nim
+++ main.nim
@@ -79,3 +79,9 @@ main()
 # TODO check if the commit contains a period at the end
 # TODO check if the commit contains a non imperative verb at the beginning
 # TODO check if the commit contains a pronoun
+# TODO allow the user to check all commits 
+# TODO allow the user to check a specific commit (commit hash)
+# TODO allow the user to check a range of commits
+# TODO nimpretty to pipeline
+# TODO add a test suite
+# TODO add nimit to the pipeline to check the commit message! Nimit checks nimit!
