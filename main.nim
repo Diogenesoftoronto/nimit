12543e3cf93accbc4ef8f0523ad400bece0f2380
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-03-25 23:05:48 -0400

    make main simple by removing commit procs

--- main.nim
+++ main.nim
@@ -1,62 +1,8 @@
 # import rdstdin
-import strutils
 import os
-# import re
-# import strformat
-# import sequtils
 import typetraits
 import system
-import osproc
-import parsecsv
-
-# Exceptions
-var
-    NOGIT: ref CatchableError
-new(NOGIT)
-NOGIT.msg = "No git directory is present"
-
-proc getLatestCommit(): string {.raises: [OSError, IOError].} =
-    let cmd = "git log -1 > tmp.commit"
-    var ok = execCmd(cmd)
-    if ok != 0:
-        raise new(OSError)
-    try:
-        var f = open("./tmp.commit")
-        defer: f.close()
-        var lineNumber = 0
-        for line in f.lines:
-            inc lineNumber
-            if lineNumber == 5:
-                result = line
-    except IOError as e:
-        raise e
-
-proc validateCommit(commit: string): bool {.raises: [OSError, IOError, Exception].} =
-    result = false
-    var p: CsvParser
-    var q: CsvParser
-    p.open("./data/pronouns.csv")
-    q.open("./data/verbs.csv")
-    defer: p.close()
-    defer: q.close()
-    # check if sentence begins with a verb
-    p.readHeaderRow
-    q.readHeaderRow
-    while q.readRow():
-        for col in items(q.headers):
-            if col != "Word":
-                continue
-            var val = q.rowEntry(col)
-            # remove empty spaces from a string
-            if commit.strip.startsWith(val):
-                result = true
-                echo "Valid commit message"
-                break
-    if result == false:
-        echo "Invalid commit message"
-        result = false
-        raise new(OSError)
-
+import ./commit
 
 proc main() =
     # try:
@@ -67,21 +13,21 @@ proc main() =
         echo "No Git directory found"
         raise NOGIT
     try:
-        var msg = getLatestCommit()
-        validateCommit(msg)
+        # var file = "tmp.commit"
+        createCommitFile()
+        var procs = getCommitProcs()
+        for p in procs:
+            discard validateCommitProc(p)
     except OSError, IOError:
         raise
 
 main()
 
-# TODO check if the commit is capitalized
-# TODO check if the commit is more than 50 characters
-# TODO check if the commit contains a period at the end
 # TODO check if the commit contains a non imperative verb at the beginning
 # TODO check if the commit contains a pronoun
-# TODO allow the user to check all commits 
-# TODO allow the user to check a specific commit (commit hash)
+# TODO allow the user to check all commits
 # TODO allow the user to check a range of commits
 # TODO nimpretty to pipeline
 # TODO add a test suite
 # TODO add nimit to the pipeline to check the commit message! Nimit checks nimit!
+# TODO change csv columns to enums with a macro
